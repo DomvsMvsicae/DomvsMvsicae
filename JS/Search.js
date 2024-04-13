5a65a200871123de4ccc9349b92af6222c70c11c
GXC1130 <78024408+GXC1130@users.noreply.github.com>
2024-04-13 20:30:58 +0200

    Version 1.0
    - Fixed search.js

--- JS/Search.js
+++ JS/Search.js
@@ -1,16 +1,18 @@
-document.getElementById("searchInput").addEventListener("input", function() {
-    // Trim method deletes blank spaces at the beginning or at the end of the string
-    var searchText = this.value.trim().toLowerCase();
-    // items stores all the elements that are part of the class MusicPieceBlock
-    var items = document.querySelectorAll(".MusicPieceBlock");
-    items.forEach(function(item) {
-        var itemText = item.textContent.toLowerCase();
-        if (searchText === "") {
-            item.style.display = "flex"; // Diplays every music piece if there's no input
-        } else if (itemText.includes(searchText)) {
-            item.style.display = "flex"; // Displays music pieces matching the input
-        } else {
-            item.style.display = "none"; 
-        }
+window.onload = function(){
+    document.getElementById("searchInput").addEventListener("input", function() {
+        // Trim method deletes blank spaces at the beginning or at the end of the string
+        var searchText = this.value.trim().toLowerCase();
+        // items stores all the elements that are part of the class MusicPieceBlock
+        var items = document.querySelectorAll(".MusicPieceBlock");
+        items.forEach(function(item) {
+            var itemText = item.textContent.toLowerCase();
+            if (searchText === "") {
+                item.style.display = "flex"; // Diplays every music piece if there's no input
+            }else if (itemText.includes(searchText)) {
+                item.style.display = "flex"; // Displays music pieces matching the input
+            }else {
+                item.style.display = "none"; 
+            }
+        });
     });
-});
+}
